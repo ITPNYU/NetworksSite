3a6bec36c3483bfdb2cc91da31b38074c58d5350
viztopia <37890050+viztopia@users.noreply.github.com>
2020-05-19 13:22:43 -0400

    add CORS proxy example

---
+++ Intro-to-CORS/README.md
@@ -0,0 +1,8 @@
+# An-Introduction-to-CORS
+Example code for the "An Introduction to CORS" blog post on ITP Networks site. 
+(Link will be provided once the blog post is published.)
+
+- You will need to have Node.js installed in order to run the examples.
+- Instructions for using the examples:
+1. Open terminal, cd to the proxy (or implement-cors) folder, and install the library dependences by entering: npm install
+2. Run the server by entering: node server.js
---
+++ Intro-to-CORS/proxy/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); //use res.header() to add the Access-Control-Allow-Origin: * header to all responses
+  next();
+});
+
+app.get('/', (req, res) => {
+  request(
+    {url: 'https://joke-api-strict-cors.appspot.com/jokes/random'}, //here you specify the url to the resource (an image, API, etc.) that does not support CORS
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      res.json(JSON.parse(body));
+    }
+  )
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`CORS proxy listening on port ${PORT}`));
